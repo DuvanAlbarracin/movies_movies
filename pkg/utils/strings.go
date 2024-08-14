06c60c2b0b7c0c51c9d9523239439d08265ab73e
Duvan Albarracin <duvan.albarracin@soyveci.com>
2024-08-14 16:40:01 -0500

    added toSnakeCase fuction to handle mismatch table names, renamed utils file

--- pkg/utils/strings.go
+++ pkg/utils/strings.go
@@ -36,3 +36,31 @@ func BuildUpdateQueryString(table string, id int64, fields map[string]any) (stri
 
 	return b.String(), nil
 }
+
+func ToSnakeCase(s string) string {
+	var sb strings.Builder
+	offset := 0
+
+	if isUpper(s[0]) {
+		offset = 32
+	}
+	sb.WriteByte(s[0] + byte(offset))
+
+	for i := 1; i < len(s); i++ {
+		offset = 0
+		if isUpper(s[i]) {
+			offset = 32
+			sb.WriteRune('_')
+		}
+		sb.WriteByte(s[i] + byte(offset))
+	}
+
+	return sb.String()
+}
+
+func isUpper(b byte) bool {
+	if int(b) > 65 && int(b) < 90 {
+		return true
+	}
+	return false
+}
